7f1d22c0557d563ddee6efaf773f773c39792204
pandafixit <146379334+pandafixit@users.noreply.github.com>
2023-09-28 20:43:07 +0200

    Update Ownable.sol

--- contracts/access/Ownable.sol
+++ contracts/access/Ownable.sol
@@ -1,100 +1,120 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v4.9.0) (access/Ownable.sol)
+pragma solidity ^0.8.0;
 
-pragma solidity ^0.8.20;
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-import {Context} from "../utils/Context.sol";
+contract PandaFixToken is ERC20Burnable, Ownable {
+    using SafeMath for uint256;
 
-/**
- * @dev Contract module which provides a basic access control mechanism, where
- * there is an account (an owner) that can be granted exclusive access to
- * specific functions.
- *
- * The initial owner is set to the address provided by the deployer. This can
- * later be changed with {transferOwnership}.
- *
- * This module is used through inheritance. It will make available the modifier
- * `onlyOwner`, which can be applied to your functions to restrict their use to
- * the owner.
- */
-abstract contract Ownable is Context {
-    address private _owner;
+    string private _name = "PandaFix"; // Customize: Token Name
+    string private _symbol = "PFX"; // Customize: Token Symbol
+    uint8 private _decimals = 18; // Customize: Number of Decimals
+    uint256 private _totalSupply = 1000000000 * 10**uint256(_decimals); // Customize: Total Supply
+    uint256 private _maxBuyAmount = _totalSupply.mul(5).div(1000); // 0.5% of total supply
+    uint256 private _maxSellAmount = _totalSupply.mul(5).div(1000); // 0.5% of total supply
+    uint256 private _developerFeePercent = 5; // 0.5% Customize: Developer Fee Percentage
+    uint256 private _liquidityFeePercent = 5; // 0.5% Customize: Liquidity Fee Percentage
+    uint256 private _investorFeePercent = 10; // 1% Customize: Investor Fee Percentage
+    address private _developerWallet = address(0xYourDeveloperWallet); // Customize: Developer Wallet Address
+    address private _liquidityWallet = address(0xYourLiquidityWallet); // Customize: Liquidity Wallet Address
+    address private _investorWallet = address(0xYourInvestorWallet); // Customize: Investor Wallet Address
+    bool private _developerWalletLocked = false;
+    bool private _liquidityWalletLocked = false;
+    bool private _investorWalletLocked = false;
 
-    /**
-     * @dev The caller account is not authorized to perform an operation.
-     */
-    error OwnableUnauthorizedAccount(address account);
-
-    /**
-     * @dev The owner is not a valid owner account. (eg. `address(0)`)
-     */
-    error OwnableInvalidOwner(address owner);
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    /**
-     * @dev Initializes the contract setting the address provided by the deployer as the initial owner.
-     */
-    constructor(address initialOwner) {
-        if (initialOwner == address(0)) {
-            revert OwnableInvalidOwner(address(0));
-        }
-        _transferOwnership(initialOwner);
-    }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        _checkOwner();
+    // Anti-whale policy
+    modifier checkBuyAmount(uint256 amount) {
+        require(amount <= _maxBuyAmount || msg.sender == owner(), "Buy amount exceeds the limit");
         _;
     }
 
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view virtual returns (address) {
-        return _owner;
+    modifier checkSellAmount(uint256 amount) {
+        require(amount <= _maxSellAmount || msg.sender == owner(), "Sell amount exceeds the limit");
+        _;
     }
 
-    /**
-     * @dev Throws if the sender is not the owner.
-     */
-    function _checkOwner() internal view virtual {
-        if (owner() != _msgSender()) {
-            revert OwnableUnauthorizedAccount(_msgSender());
-        }
+    constructor() ERC20(_name, _symbol) {
+        _mint(msg.sender, _totalSupply);
     }
 
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby disabling any functionality that is only available to the owner.
-     */
-    function renounceOwnership() public virtual onlyOwner {
-        _transferOwnership(address(0));
+    function transfer(address recipient, uint256 amount)
+        public
+        override
+        checkSellAmount(amount)
+        returns (bool)
+    {
+        require(!_developerWalletLocked || msg.sender == owner(), "Developer wallet is locked");
+        require(!_liquidityWalletLocked || msg.sender == owner(), "Liquidity wallet is locked");
+        require(!_investorWalletLocked || msg.sender == owner(), "Investor wallet is locked");
+
+        uint256 fee = calculateFee(amount);
+        uint256 transferAmount = amount.sub(fee);
+
+        super.transfer(_developerWallet, fee.mul(_developerFeePercent).div(100));
+        super.transfer(_liquidityWallet, fee.mul(_liquidityFeePercent).div(100));
+        super.transfer(_investorWallet, fee.mul(_investorFeePercent).div(100));
+        super.transfer(recipient, transferAmount);
+
+        return true;
     }
 
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        if (newOwner == address(0)) {
-            revert OwnableInvalidOwner(address(0));
-        }
-        _transferOwnership(newOwner);
+    function calculateFee(uint256 amount) private view returns (uint256) {
+        return amount.mul(2).div(100); // 2% fee
     }
 
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Internal function without access restriction.
-     */
-    function _transferOwnership(address newOwner) internal virtual {
-        address oldOwner = _owner;
-        _owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
+    function lockDeveloperWallet() external onlyOwner {
+        _developerWalletLocked = true;
+    }
+
+    function unlockDeveloperWallet() external onlyOwner {
+        _developerWalletLocked = false;
+    }
+
+    function lockLiquidityWallet() external onlyOwner {
+        _liquidityWalletLocked = true;
+    }
+
+    function unlockLiquidityWallet() external onlyOwner {
+        _liquidityWalletLocked = false;
+    }
+
+    function lockInvestorWallet() external onlyOwner {
+        _investorWalletLocked = true;
+    }
+
+    function unlockInvestorWallet() external onlyOwner {
+        _investorWalletLocked = false;
+    }
+
+    function isDeveloperWalletLocked() external view returns (bool) {
+        return _developerWalletLocked;
+    }
+
+    function isLiquidityWalletLocked() external view returns (bool) {
+        return _liquidityWalletLocked;
+    }
+
+    function isInvestorWalletLocked() external view returns (bool) {
+        return _investorWalletLocked;
+    }
+
+    // Set maximum buy and sell amounts (onlyOwner)
+    function setMaxBuyAmount(uint256 amount) external onlyOwner {
+        _maxBuyAmount = amount;
+    }
+
+    function setMaxSellAmount(uint256 amount) external onlyOwner {
+        _maxSellAmount = amount;
+    }
+
+    // Get maximum buy and sell amounts
+    function getMaxBuyAmount() external view returns (uint256) {
+        return _maxBuyAmount;
+    }
+
+    function getMaxSellAmount() external view returns (uint256) {
+        return _maxSellAmount;
     }
 }
